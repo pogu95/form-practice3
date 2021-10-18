f9292feccb2259e39b722456ce1666155b00c601
missbolin <samanthabolin1@gmail.com>
2021-10-17 20:14:20 -0700

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+router.post('/results', function(req, res){
+  console.log(req.body.description);
+  console.log(req.body.agree);
+  console.log(req.body.icecream);
+  console.log(req.body.iceCreamOther);
+  console.log(req.body.text);
+  console.log(req.body.dateOfBirth);
+  res.render('card', {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    dateOfBirth: new Date(req.body.dateOfBirth),
+    description: req.body.description,
+    agree: req.body.agree,
+    icecream: req.body.icecream
+  })
+});
+
+
 
 module.exports = router;
